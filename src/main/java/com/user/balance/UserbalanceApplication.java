0d831697604fd4f83c6127a555ac7f9efe804952
Roberto Gonçalves <roberto.forsaken@gmail.com>
2019-05-29 19:36:35 -0300

    initial

---
+++ src/main/java/com/user/balance/UserbalanceApplication.java
@@ -0,0 +1,13 @@
+package com.user.balance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserbalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserbalanceApplication.class, args);
+	}
+
+}
